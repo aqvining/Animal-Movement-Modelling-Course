509b80946cca126a10dcb9db3477050021fe9a62
Alexander Vining <aqvining@ucdavis.edu>
2017-10-16 14:07:00 -0700

    = to ->

--- Week 3 Script.R
+++ Week 3 Script.R
@@ -6,9 +6,9 @@ move <- function(location, steps) {
   #output: a data frame with 10 new x,y coordinates
   #description: in each step, the agent can move 1 unit in any cardinal direction, or remain still.
   if (!identical(names(location), c("x","y"))) print("location should be a data frame with two columns named 'x' and 'y'")
-  newX = location$x + sample(-1:1, size = 1) #sample takes a random draw from a vector of numbers
-  newY = location$y + sample(-1:1, size = 1)
-  newLocation = data.frame(x = newX, y = newY)
+  newX <- location$x + sample(-1:1, size = 1) #sample takes a random draw from a vector of numbers
+  newY <-  location$y + sample(-1:1, size = 1)
+  newLocation <- data.frame(x = newX, y = newY)
   if (steps == 0) {return(newLocation) #Base Case: function stops when there are no more steps to take
   } else {
     return(rbind(location, move(newLocation, steps - 1))) #rbind takes two dataframes (or a df and a vector) with the same columns and combines them
